c2b484c0a10af90e97b7ef6f6ba6750dd2d76d26
Erick M. Hernández Quiñones <erick.hernandez@magestree.com>
2023-10-03 12:25:08 +0200

    Bump version and refactor environment loading functionality
    Upgraded python_fortress from version 0.2.0 to 0.2.1 in setup.py. The Fortress class has been refactored to load configuration credentials through a more straightforward and flexible process. This involves removing find_dotenv() function, additional functionalities to implement loading and configuring credentials directly in the constructor and directly through the configure() method.
    
    The changes also involved revising README.md to reflect the new way of using the library and update corresponding test cases in test_fortress.py. This refactor enhances code readability and usability of the library by users.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Python Fortress
 
-Python Fortress is a library designed to facilitate interaction with an API to retrieve an .env file. It's designed for securely and efficiently loading environment variables from a remote source.
+Python Fortress is a library designed to facilitate interaction with an API to retrieve an .env file and load its values into your environment variables. It's designed for securely and efficiently loading environment variables from a remote source.
 
 [![codecov](https://codecov.io/gh/magestree/python_fortress/branch/develop/graph/badge.svg)](https://codecov.io/gh/magestree/python_fortress)
 
 ## Features
 
-- Easy loading of credentials and configurations from environment variables.
-- Secure retrieval of the `.env` file content from an API.
+- Easy loading and configuration.
+- Secure retrieval of the `.env` file content from [passfortress.com](https://www.passfortress.com) API.
 - Simple and efficient usage with intuitive methods.
 
 ## Installation
@@ -20,20 +20,30 @@ pip install -r requirements.txt
 
 ## Basic usage
 
-The main module `fortress.py` provides the `Fortress` class and a convenience function `load_env()`.
+The main module `fortress.py` provides the `Fortress` class and a convenience function `configure()` and `load_env()`.
+You just need configure your `Fortress` instance and then, call the `load_env()` method with desired envfile `id`.
 
 ## Example:
 
 ```python
-from python_fortress.fortress import load_env
+from python_fortress.fortress import Fortress
 
-load_env()
+credentials = {
+    "api_key": "<YOUR_API_KEY>",
+    "access_token": "<YOUR_ACCESS_TOKEN>",
+    "master_key": "<YOUR_MASTER_KEY>"
+}
+
+fortress = Fortress()
+fortress.configure(**credentials)
+fortress.load_env(envfile_id="<YOUR_ENVFILE_ID>")
 ```
 
 ## Tests
 The project comes with a set of tests to ensure everything works as expected. You can run the tests using pytest:
 
 ```bash
+pip install -r requirements_dev.txt
 pytest
 ```
 
--- python_fortress/fortress.py
+++ python_fortress/fortress.py
@@ -1,10 +1,9 @@
 import logging
-import os
 from io import StringIO
-from typing import Dict, Optional
+from typing import Optional
 
 import requests
-from dotenv import dotenv_values, load_dotenv
+from dotenv import load_dotenv
 
 # logging configuration
 logging.basicConfig(level=logging.INFO)
@@ -13,53 +12,33 @@ logger = logging.getLogger(__name__)
 BASE_URL = "https://www.passfortress.com/api/"
 
 
-def find_dotenv(filename=".env", raise_error_if_not_found=False, usecwd=False):
-    current_dir = os.getcwd() if usecwd else os.path.dirname(os.path.abspath(__file__))
-    while current_dir != os.path.dirname(current_dir):
-        env_path = os.path.join(current_dir, filename)
-        if os.path.exists(env_path):
-            return env_path
-        current_dir = os.path.dirname(current_dir)
-    if raise_error_if_not_found:
-        raise FileNotFoundError(f"Could not find {filename} file.")
-    return None
-
-
 class Fortress:
+    api_key: str
+    access_token: str
+    master_key: str
+    envfile_name: str
+
     """
     A class that represents a secret fortress and allows you to interact with the API to retrieve an .env file.
     """
 
-    def __init__(self, base_url: str = BASE_URL):
+    def __init__(self, base_url: str = BASE_URL, **credentials):
         """
         Initializes a Fortress instance.
 
         :param base_url: URL base para la API.
         """
-        self.base_url = base_url
-        self.api_key: str = ""
-        self.access_token: str = ""
-        self.master_key: str = ""
-        self.envfile_name: str = ""
-        self.headers: Dict[str, str] = {}
-        self.load_fortress_credentials()
-        self.load_headers()
+        self.base_url: str = base_url
+        self.configure(**credentials)
 
-    def load_fortress_credentials(self, dotenv_loader=dotenv_values) -> None:
-        """
-        Load credentials from environment variables using python-dotenv.
-        """
-        fortress_credentials = dotenv_loader(find_dotenv())
-        self.api_key = fortress_credentials.get("FORTRESS_API_KEY", "")
-        self.access_token = fortress_credentials.get("FORTRESS_ACCESS_TOKEN", "")
-        self.master_key = fortress_credentials.get("FORTRESS_MASTER_KEY", "")
-        self.envfile_name = fortress_credentials.get("FORTRESS_ENVFILE_NAME", "")
+    @property
+    def headers(self):
+        return {"Authorization": f"Bearer {self.access_token}"}
 
-    def load_headers(self) -> None:
-        """
-        Loads the necessary headers for API requests.
-        """
-        self.headers = {"Authorization": f"Bearer {self.access_token}"}
+    def configure(self, **credentials):
+        self.api_key: str = credentials.get("api_key", None)
+        self.access_token: str = credentials.get("access_token", None)
+        self.master_key: str = credentials.get("master_key", None)
 
     def _build_url(self, endpoint: str) -> str:
         """
@@ -70,37 +49,28 @@ class Fortress:
         """
         return f"{self.base_url}{endpoint}"
 
-    def get_envfile(self) -> Optional[str]:
+    def get_envfile(self, envfile_id) -> Optional[str]:
         """
         Gets the content of the .env file from the API.
 
         :return: Content of the .env file or None if there is an error.
         """
-        url = self._build_url(endpoint="get-envfile/")
+        url = self._build_url(endpoint=f"get-secret/{envfile_id}/")
         data = {
             "api_key": self.api_key,
             "master_key": self.master_key,
-            "envfile_name": self.envfile_name,
+            "secret_type": "envfile",
         }
         response = requests.post(url=url, data=data, headers=self.headers)
         if response.status_code == 200:
-            return response.json().get("envfile_data", {}).get("value")
+            return response.json().get("secret_data", {}).get("value")
 
         logger.error(f"Error getting envfile. Code: {response.status_code}. message: {response.text}")
         return None
 
-    def load_env(self):
-
-        envfile = self.get_envfile()
+    def load_env(self, envfile_id):
+        envfile = self.get_envfile(envfile_id)
         if envfile:
             load_dotenv(stream=StringIO(envfile))
             return True
         return False
-
-
-def load_env():
-    """
-    Convenience function to load the variables from the .env file into the environment variables.
-    """
-    fortress = Fortress()
-    fortress.load_env()
